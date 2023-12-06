aee79c5bed18bd0f5fb6455a7a3033bc9feb08ad
Agustin Banchio <agustin@banch.io>
2023-12-06 23:01:41 +0000

    day 7 skeleton

---
+++ day_7/part_one.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	fmt.Println("Advent of Code 2023 - Day 7")
+
+	inputBytes, _ := os.ReadFile("input.txt")
+	input := string(inputBytes)
+
+	lines := strings.Split(input, "\n")
+
+	total := 0
+
+	for _, line := range lines {
+
+		if line == "" {
+			continue
+		}
+
+	}
+
+	fmt.Println("Total:", total)
+}
